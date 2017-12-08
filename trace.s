5c323b26317a169ed8ebbdb9f923710b20f8e379
Erik Hemming <eriquesnk@gmail.com>
2017-12-08 15:53:49 +0100

    work shared with mike

--- trace.s
+++ trace.s
@@ -8,7 +8,7 @@
 ; ==> 80 bytes
 
 
-
+; compact mode = 4 longs = 16 bytes
 ; extended mode = 80 bytes
 
 		; compare Dn + An + USP
@@ -20,8 +20,8 @@
 
 		jmp	S
 
-START_OPCODE	= $0241
-OPCODE_COUNT	= $1
+START_OPCODE	= $0000
+OPCODE_COUNT	= $10000
 
 REG_An_DELTA	= $100
 
@@ -45,7 +45,7 @@ REG_A3 = BASE_ADDRESS+REG_An_DELTA*6
 REG_A4 = BASE_ADDRESS+REG_An_DELTA*7
 REG_A5 = BASE_ADDRESS+REG_An_DELTA*8
 REG_A6 = BASE_ADDRESS+REG_An_DELTA*9
-REG_SSP= BASE_ADDRESS+REG_An_DELTA*10
+REG_A7 = BASE_ADDRESS+REG_An_DELTA*10
 REG_USP= BASE_ADDRESS+REG_An_DELTA*11
 
 
@@ -81,29 +81,38 @@ S
 		movem.l	d1-a6,-(sp)
 		move.l	#START_OPCODE,d0	; start opcode
 		move.l	#OPCODE_COUNT-1,d7
-		lea	Compact,a0
+		lea	Results,a4
 
 .tryOp
 		move.w	d0,$dff180		; some visual feedback (COLOR00)
 
-		moveq.l	#$0000,d1		; USER mode, CCR = $0
-		bsr.b	Execute			; Execute opcode d0.w in SR mode d1.w
+		moveq.l	#$0000,d1		; USER mode
+.execute	move.l	#Execute,$80.w		; Execute one instruction
+		trap	#0			; switch to SUPER
 
-;		move.w	VectorState(pc),d1
-;		and.w	#1<<4,d1
-;		bne.b	.notLegal
-;		move.w	d0,(a0)+
-;.notLegal
+		move.w	VectorState(pc),d1
+		and.w	#1<<8,d1
+		beq.b	.notPriv
 
-		bsr.w	WriteCompactState
-		adda.w	#16,a0
+		cmp.w	#$4e70,d0		; $4e70 = RESET
+		beq.b	.notPriv
 
+		or.w	#$2000,d1		; run in SUPER
+		bra.b	.execute
+.notPriv	
+		move.w	VectorState(pc),d1
+		and.w	#1<<4,d1
+		bne.b	.notLegal
+
+		move.w	d0,(a4)+
+
+.notLegal
 		addq.l	#1,d0
 		dbf	d7,.tryOp
 
-;		suba.l	#Compact,a0
-;		move.l	a0,d0
-;		lsr.l	#1,d0
+		suba.l	#Results,a4
+		move.l	a4,d0
+		lsr.l	#1,d0
 
 		movem.l	(sp)+,d1-a6
 		rts
@@ -116,31 +125,12 @@ S
 ; d1.w = SR
 
 Execute:
-		move.l	#.execute,$80.w		; Execute one instruction
-		trap	#0			; switch to SUPER
-
-		move.w	VectorState(pc),d1
-		and.w	#1<<8,d1		; check for PRIV violation
-		beq.b	.done
-
-		cmp.w	#$4e70,d0		; $4e70 = RESET
-		beq.b	.done
-
-		or.w	#$2000,d1		; run in SUPER
-		bra.b	Execute
-
-.done		rts
-
-.execute	move.w	#$2700,sr
+		move.w	#$2700,sr
 
 		move.w	d0,.p
 
 		movem.l	d0-a6,-(sp)
 		move.l	a7,SuperStack
-
-		eor.w	#$1000,sr		; enable ISP
-		move.l	a7,InterruptStack
-		eor.w	#$1000,sr		; disable ISP
 
 		move.l	USP,a0
 		move.l	a0,UserStack
@@ -160,15 +150,10 @@ Execute:
 		bsr.w	ClearState
 		bsr.w	SetupRegisters
 
-;		bra.b	Cleanup
-
 .smc:		move.w	#$ffff,sr		; dummy write - smc
 .p:		dc.l	0,0,0,0,0
 
-a7check		dc.l	0
-
 Cleanup:	
-		move.l	a7,a7check
 		; compare Dn + An + USP
 		; Dx.l + Ax.l + USP.l
 		; SR.w + EX.w + PC.w
@@ -189,96 +174,30 @@ Cleanup:
 		move.l	a4,RegState_A4
 		move.l	a5,RegState_A5
 		move.l	a6,RegState_A6
-		move.l	a7,RegState_SSP
-		add.l	#12,RegState_SSP	; normal RTE frame
+		move.l	a7,RegState_A7
 		move.l	USP,a0
 		move.l	a0,RegState_USP
 
 		move.l	ProgramCounter(pc),d0
 		sub.l	#Execute\.p,d0
-		cmp.l	#32,d0			; let's assume no instruction is longer than 32bytes ;)
-		blo.b	.sizeOk
-		move.w	#$ffff,d0
-.sizeOk		move.w	d0,InstructionSize
+		move.w	d0,InstructionSize
+
+		bsr.w	RestoreVectors
 
 		move.l	UserStack(pc),a0
 		move.l	a0,USP
 
-		eor.w	#$1000,sr		; enable ISP
-		move.l	InterruptStack(pc),a7
-		eor.w	#$1000,sr		; disable ISP
-
 		move.l	SuperStack(pc),a7
-
-		bsr.w	RestoreVectors
-
 		movem.l	(sp)+,d0-a6
 		rte
 
 SuperStack	dc.l	0
-InterruptStack	dc.l	0
 UserStack	dc.l	0
 
 ClearState	lea	State(pc),a0
 		moveq.l	#(StateEnd-State)/4-1,d7
 .clear		move.l	#0,(a0)+
 		dbf	d7,.clear
-		rts
-
-
-WriteCompactState
-	; a0.l = state
-	;
-	; compare current state against the 'ideal' state
-	; output 16 bytes compact mode state
-	;
-	; VECT|TRAP + SR|Isize + Dn|An + CRC32mem
-	;
-		movem.l	d0-d1,-(sp)
-
-		move.w	VectorState(pc),(a0)
-		move.w	TrapState(pc),2(a0)
-		move.w	StatusRegister(pc),4(a0)
-		move.w	InstructionSize(pc),6(a0)
-
-		moveq.l	#0,d0
-		moveq.l	#1,d1
-
-CompareReg	MACRO
-		cmp.l	#\1,\2
-		beq.b	*+4
-		or.l	d1,d0
-		add.l	d1,d1
-		ENDM
-
-		CompareReg	REG_D0,RegState_D0
-		CompareReg	REG_D1,RegState_D1
-		CompareReg	REG_D2,RegState_D2
-		CompareReg	REG_D3,RegState_D3
-		CompareReg	REG_D4,RegState_D4
-		CompareReg	REG_D5,RegState_D5
-		CompareReg	REG_D6,RegState_D6
-		CompareReg	REG_D7,RegState_D7
-
-		CompareReg	REG_A0,RegState_A0
-		CompareReg	REG_A1,RegState_A1
-		CompareReg	REG_A2,RegState_A2
-		CompareReg	REG_A3,RegState_A3
-		CompareReg	REG_A4,RegState_A4
-		CompareReg	REG_A5,RegState_A5
-		CompareReg	REG_A6,RegState_A6
-		CompareReg	REG_USP,RegState_USP
-
-		move.l	d0,8(a0)
-
-		moveq.l	#0,d0
-		move.l	#$8000,d1
-		CompareReg	REG_SSP,RegState_SSP
-		or.w	d0,(a0)			; reuse Vector15 to signal SSP change
-
-		move.l	#$01234567,12(a0)
-
-		movem.l	(sp)+,d0-d1
 		rts
 
 		cnop	0,4
@@ -288,7 +207,6 @@ TrapState:	dc.w	0
 StatusRegister	dc.w	0
 InstructionSize:dc.w	0
 ProgramCounter:	dc.l	0
-		dc.l	0	; pad
 RegState_D0	dc.l	0
 RegState_D1	dc.l	0
 RegState_D2	dc.l	0
@@ -304,13 +222,13 @@ RegState_A3	dc.l	0
 RegState_A4	dc.l	0
 RegState_A5	dc.l	0
 RegState_A6	dc.l	0
-RegState_SSP	dc.l	0
+RegState_A7	dc.l	0
 RegState_USP	dc.l	0
 		cnop	0,4
 StateEnd:
 
 SetupRegisters:
-		move.l	(a7)+,REG_SSP-4
+		move.l	(a7)+,REG_A7-4
 		move.l	#REG_D0,d0
 		move.l	#REG_D1,d1
 		move.l	#REG_D2,d2
@@ -330,14 +248,14 @@ SetupRegisters:
 		movea.l	#REG_USP,a7
 		move.l	a7,USP
 		eor.w	#$1000,sr		; enable ISP
-		movea.l	#REG_SSP-4,a7		; == ISP
+		movea.l	#REG_A7-4,a7		; == ISP
 		eor.w	#$1000,sr		; disable ISP
-		movea.l	#REG_SSP-4,a7		; == SSP
+		movea.l	#REG_A7-4,a7		; == SSP
 
 		; Fake exception frame
 
-		move.w	#$0000,REG_SSP
-		move.l	#Execute\.p+2,REG_SSP+2
+		move.w	#$0000,REG_A7
+		move.l	#Execute\.p+2,REG_A7+2
 
 		move	#0,ccr
 		rts
@@ -357,7 +275,7 @@ ClearMemory:	move.w	#REG_An_DELTA/4-1,d7
 		movea.l	#REG_A4,a4
 		movea.l	#REG_A5,a5
 		movea.l	#REG_A6,a6
-		movea.l	#REG_SSP,a7
+		movea.l	#REG_A7,a7
 
 .clear:
 	rept 4
@@ -368,8 +286,8 @@ ClearMemory:	move.w	#REG_An_DELTA/4-1,d7
 		move.l	d0,(a4)+
 		move.l	d0,(a5)+
 		move.l	d0,(a6)+
-		move.l	d0,(a7)+	; SSP
 		move.l	d0,(a7)+	; USP
+		move.l	d0,(a7)+	; SSP
 	endr
 		dbf	d7,.clear
 		movea.l	d1,a7
@@ -393,23 +311,18 @@ SystemVectors:	ds.l	32+16
 
 
 SetupExceptionHandlers:
-		move.l	#$EDB88320,d0
-		lea	$0.w,a0
-		move.l	d0,(a0)+		; Reset SP
-		move.l	d0,(a0)+		; Reset PC
+
+		lea	$8.w,a0
 		moveq.l	#10-1,d7
 		lea	Vector2(pc),a1
-.initVectors:	move.l	a1,(a0)+		; Vector2 - Vector11
+.initVectors:	move.l	a1,(a0)+
 		adda.w	#Vector3-Vector2,a1		
 		dbf	d7,.initVectors
 
-		moveq.l	#20-1,d7
-.initDummies	move.l	d0,(a0)+		; Vector20 - Vector31
-		dbf	d7,.initDummies
-
+		lea	$80.w,a0
 		moveq.l	#16-1,d7
 		lea	Trap0(pc),a1
-.initTraps:	move.l	a1,(a0)+		; Vector32 - Vector47
+.initTraps:	move.l	a1,(a0)+
 		adda.w	#Trap1-Trap0,a1		
 		dbf	d7,.initTraps
 
@@ -499,4 +412,4 @@ E:
 
 	section opc,bss_f
 
-Compact		ds.l	OPCODE_COUNT*4	; 4 longs per op, in compact mode
+Results		ds.w	OPCODE_COUNT*4
